ff4f16e75474dfcc544688d7850e79b8557e30bb
alvinati <choirunisata@gmail.com>
2021-12-13 15:40:32 +0700

    move set applocation

--- src/test/java/config/TestConfig.kt
+++ src/test/java/config/TestConfig.kt
@@ -8,16 +8,21 @@ import io.appium.java_client.remote.MobileCapabilityType
 import org.openqa.selenium.remote.DesiredCapabilities
 import java.net.URL
 
-/**
- * WARNING!!
- * please be careful when moving any line of code here as these lines will be rewritten in bitrise
- */
+
 object TestConfig {
-   private val platformType = PlatformType.iOS
+
+   /**
+    * WARNING!!
+    * please be careful when moving these lines of code here, as these lines will be rewritten in bitrise
+    * current lines modified in bitrise:
+    * 20,21
+    */
+   private val platformType = PlatformType.Android
+   private val appLocation = "/Users/choir/Downloads/gitsearch.apk"
 
    private val url = URL("http://127.0.0.1:4723/wd/hub")
 
-   const val SCREEN_WAIT_TIMEOUT = 30L
+   const val SCREEN_WAIT_TIMEOUT = 180L
 
    fun getAppTestDriver(): AppiumDriver<out MobileElement> {
       return if(platformType == PlatformType.Android){
@@ -31,9 +36,9 @@ object TestConfig {
          setCapability(MobileCapabilityType.DEVICE_NAME, "Android Emulator")
          setCapability(MobileCapabilityType.PLATFORM_NAME, "Android")
          setCapability(MobileCapabilityType.PLATFORM_VERSION, "10")
-         setCapability(MobileCapabilityType.APP, System.getenv("BITRISE_APK_PATH"))
-         //setCapability(MobileCapabilityType.APP, "/Users/choir/Downloads/release/app-release.apk")
+         setCapability(MobileCapabilityType.APP, appLocation)
          setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2")
+         setCapability(MobileCapabilityType.NO_RESET, true)
          setCapability("androidInstallTimeout", 600000)
          setCapability("adbExecTimeout", 600000)
       }
@@ -46,8 +51,9 @@ object TestConfig {
          setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 11")
          setCapability(MobileCapabilityType.PLATFORM_NAME, "iOS")
          setCapability(MobileCapabilityType.PLATFORM_VERSION, "14.5")
-         setCapability(MobileCapabilityType.APP, System.getenv("BITRISE_APP_DIR_PATH"))
+         setCapability(MobileCapabilityType.APP, appLocation)
          setCapability(MobileCapabilityType.AUTOMATION_NAME, "XCUITest")
+         setCapability(MobileCapabilityType.NO_RESET, true)
          setCapability("useNewWDA", false)
       }
       return IOSDriver(url, capabilities)
